d6c6952a8f0ebecfa70093141ea436c617ac2b0b
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2023-01-10 14:52:07 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-alert("Hello")
+import "css/front-page.css"
+
+// Our modules / classes
+import MobileMenu from "modules/MobileMenu"
+import HeroSlider from "modules/HeroSlider"
+import GoogleM from "modules/GoogleMap.js"
+import search from "modules/Search.js"
+
+// Instantiate a new object using our modules/classes
+const mobileMenu = new MobileMenu()
+const heroSlider = new HeroSlider()
+const GoogleM = new GoogleM()
+const search = new search()
