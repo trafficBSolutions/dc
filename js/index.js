1fe8503ec6bbd73c5f4ed9169e205274a3fbb807
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2023-01-10 14:53:07 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,5 +9,5 @@ import search from "modules/Search.js"
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu()
 const heroSlider = new HeroSlider()
-const GoogleM = new GoogleM()
-const search = new search()
+const googleM = new GoogleM()
+const sear = new search()
