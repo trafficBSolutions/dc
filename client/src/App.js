23c8b5139718f9aff3567be3dcc8ab0a36ae183c
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2023-01-26 19:04:12 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import Landing from "./pages/landing"
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import { Dashboard,Register,Landing,Error,Blog,Event,Past,Company,CP } from "./pages";
 
 function App() {
   return (
-    <div>
-   <Landing />
-   </div>
+<BrowserRouter>
+  <Routes>
+    <Route path="/dashboard" element={<Dashboard/>}/>
+    <Route path="/register" element={<Register/>}/>
+    <Route path="/landing" element={<Landing/>}/>
+    <Route path="/error" element={<Error/>}/>
+    <Route path="/blog" element={<Blog/>}/>
+    <Route path="/events" element={<Event/>}/>
+    <Route path="/past-events" element={<Past/>}/>
+    <Route path="/companies" element={<Company/>}/>
+    <Route path="/company-page" element={<CP/>}/>
+  </Routes>
+</BrowserRouter>
+
   );
 }
 
